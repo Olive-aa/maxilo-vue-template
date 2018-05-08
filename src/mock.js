338728cba358fab710747cc83fabb51ea6e29ee7
2802146520@qq.com <2802146520@qq.com>
2018-05-08 10:47:13 +0800

    maxilo
    add rap

--- src/mock.js
+++ src/mock.js
@@ -1,1 +1,17 @@
-require('./.mock');
+import mockService from 'maxilo-vue/lib/core/mocks/utils';
+
+let service = new mockService;
+
+let mocks = require('./.mock');
+
+Object.keys(mocks).forEach((k) => {
+    let build = 'resolve';
+    let v     = mocks[k];
+    if(v.reg) {
+        build = 'resolveREG';
+    }
+    service[build](k, v.build, v.type, v.title);
+});
+service.resolveRap(require('./.rap.json'));
+let raps = require('./.rap.json');
+console.log(raps);
