dc7faa91fa99b84730fe31dcb4d51d7eebd32b00
2802146520@qq.com <2802146520@qq.com>
2018-05-13 19:27:54 +0800

    maxilo
    fixed hot image

--- src/pages/test/component.vue
+++ src/pages/test/component.vue
@@ -1,22 +1,19 @@
 <template>
-    <div class="container-fluid" :style="{backgroundImage: 'url(' + c + ')', width: '500px'}">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-xs-12" :style="{backgroundImage: '(' + c + ')'}">
+            <div class="col-xs-12">
                 <form>
                     <mxl-upload v-model="q"></mxl-upload>
                 </form>
             </div>
         </div>
-        {{c}}
     </div>
 </template>
 
 <script>
-import testImg from '../../assets/images/2.png';
 export default {
     data(){
         return {
-            c: testImg,
             q: '<h3>2</h3>',
             s: [
                 {label: 123, value: 1, hasChild: 1, childs: [{label: '1-2', value: 3, hasChild: 0}]},
